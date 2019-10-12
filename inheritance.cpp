adc030cc70129a677bc4a77b14576bb58b7375e2
Alec Martin <acmartin1@live.ca>
2019-10-11 20:17:43 -0700

    Finished

--- inheritance.cpp
+++ inheritance.cpp
@@ -8,7 +8,6 @@
 #include "canine.hpp"
 
 int main() {
-    cout << "Initializing 3 Animal Pointers:" << endl;
     animal* a = new animal(1, 2,3);
     animal* b = new bird(2, 4,3,100);
     animal* c = new canine(3, 5,6);
@@ -31,7 +30,7 @@ int main() {
 
     dynamic_cast<canine*>(c)->hunt(b);
     cout << endl;
-    
+
     a->move(11, 11);
     dynamic_cast<bird*>(b)->move(10, 10, 1);
     dynamic_cast<canine*>(c)->move(10, 10);
